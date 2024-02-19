2ed46b41bff5b60ef1e43a56d30bf2202d40fc7c
51ddh4r7h <aelpulasiddharth@gmail.com>
2024-02-19 13:41:43 +0530

    minor tweaks

--- packaging-ml-model/prediction_model/processing/data_handling.py
+++ packaging-ml-model/prediction_model/processing/data_handling.py
@@ -3,22 +3,18 @@ import pandas as pd
 import joblib
 from prediction_model.config import config
 
-
-#Load the dataset
 def load_dataset(file_name):
-    filepath = os.path.join(config.DATAPATH, file_name)
-    _data = pd.read_csv(filepath)
+    file_path = os.path.join(config.DATAPATH, file_name)
+    _data = pd.read_csv(file_path)
     return _data
 
-#Serialization
-def save_pipeline(pipeline_to_save):
-    save_path = os.path.join(config.SAVE_MODEL_PATH, config.MODEL_NAME)
+def save_pipeline(pipeline_to_save, file_name):
+    save_path = os.path.join(config.SAVE_MODEL_PATH, file_name)
     joblib.dump(pipeline_to_save, save_path)
-    print("Model has been saved under the name {config.MODEL_NAME}")
+    print(f"Model has been saved under the name {file_name}")
 
-#Deserialization
-def load_pipeline(pipeline_to_load):
-    save_path = os.path.join(config.SAVE_MODEL_PATH, config.MODEL_NAME)
-    model_loaded = joblib.load(save_path)
+def load_pipeline(file_name):
+    load_path = os.path.join(config.SAVE_MODEL_PATH, file_name)
+    model_loaded = joblib.load(load_path)
     print(f"Model has been loaded")
     return model_loaded
