01c6514cfb045d711d4399d46311e5a081d1c928
51ddh4r7h <aelpulasiddharth@gmail.com>
2024-02-19 13:42:55 +0530

    *Comments

--- packaging-ml-model/prediction_model/processing/data_handling.py
+++ packaging-ml-model/prediction_model/processing/data_handling.py
@@ -3,16 +3,19 @@ import pandas as pd
 import joblib
 from prediction_model.config import config
 
+#Load the dataset
 def load_dataset(file_name):
     file_path = os.path.join(config.DATAPATH, file_name)
     _data = pd.read_csv(file_path)
     return _data
 
+#Serialization
 def save_pipeline(pipeline_to_save, file_name):
     save_path = os.path.join(config.SAVE_MODEL_PATH, file_name)
     joblib.dump(pipeline_to_save, save_path)
     print(f"Model has been saved under the name {file_name}")
 
+#Deserialization
 def load_pipeline(file_name):
     load_path = os.path.join(config.SAVE_MODEL_PATH, file_name)
     model_loaded = joblib.load(load_path)
