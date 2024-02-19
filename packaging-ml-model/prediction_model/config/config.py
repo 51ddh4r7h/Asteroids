a6f264b5db80b7565c1a7bd365f24d61d8b41ddf
51ddh4r7h <aelpulasiddharth@gmail.com>
2024-02-19 13:19:30 +0530

    Completed config.py

--- packaging-ml-model/prediction_model/config/config.py
+++ packaging-ml-model/prediction_model/config/config.py
@@ -11,4 +11,21 @@ TEST_FILE = 'test.csv'
 
 SAVE_MODEL_PATH = os.path.join(PACKAGE_ROOT, 'trained_models')
 
-TARGET = ''
+TARGET = 'hazardous'
+
+FEATURES = ['Absolute Magnitude', 'Est Dia in KM(min)', 'Relative Velocity km per sec', 'Miss Dist.(kilometers)', 'Orbit ID', 'Orbit Uncertainity', 'Minimum Orbit Intersection', 'Jupiter Tisserand Invariant', 'Epoch Osculation', 'Eccentricity', 'Inclination', 'Asc Node Longitude', 'Orbital Period', 'Perihelion Distance', 'Perihelion Arg', 'Aphelion Dist', 'Perihelion Time', 'Mean Anomaly', 'Mean Motion']
+
+NUM_FEATURES = ['Absolute Magnitude', 'Est Dia in KM(min)', 'Relative Velocity km per sec', 'Miss Dist.(kilometers)', 'Orbit ID', 'Orbit Uncertainity', 'Minimum Orbit Intersection', 'Jupiter Tisserand Invariant', 'Epoch Osculation', 'Eccentricity', 'Inclination', 'Asc Node Longitude', 'Orbital Period', 'Perihelion Distance', 'Perihelion Arg', 'Aphelion Dist', 'Perihelion Time', 'Mean Anomaly', 'Mean Motion']
+
+CAT_FEATURES = ['Orbiting Body', 'Equinox']  # Add any other categorical columns here if present in dataset
+
+FEATURES_TO_ENCODE = ['Orbiting Body', 'Equinox', 'hazardous']
+
+FEATURE_TO_MODIFY = ['Absolute Magnitude', 'Relative Velocity km per sec']
+
+FEATURE_TO_ADD = ['Distance_From_Earth']
+
+DROP_FEATURES = ['Epoch Osculation', 'Perihelion Distance']
+
+LOG_FEATURES = ['Relative Velocity km per sec', 'Miss Dist.(kilometers)']
+
